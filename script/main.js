aaadd7af1494fc759e3de9968b130cbde1a410d3
B33 <geoffrey.bii@student.moringaschool.com>
2023-06-30 22:47:56 +0300

    adding form, post and delete functions

--- script/main.js
+++ script/main.js
@@ -3,6 +3,49 @@
 
 function fetchData() {
    
+    let former = document.getElementById('new-film').addEventListener('submit', handleSubmit)
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        let filmObj ={
+        id: e.target.id.value,
+        title: e.target.title.value,
+        runtime: e.target.runtime.value,
+        capacity: e.target.capacity.value,
+        showtime: e.target.showtime.value,
+        tickets_sold: e.target.tickets_sold.value,
+        description: e.target.description.value,    
+        poster: e.target.poster.value
+        
+     }
+     postFilm(filmObj)
+    }
+    function postFilm(filmObj) {
+        fetch('http://localhost:3000/films',{
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+            body: JSON.stringify(filmObj)
+          
+        })
+        .then(res=>res.json)
+        .then(films=>console.log(films))
+    
+    }
+   
+    function deleteFilm(id) {
+        fetch(`http://localhost:3000/films/${id}`,{
+        method: 'delete',
+        headers:{
+            'Content-Type': 'application/json'
+        }
+        
+    })
+
+}
+
+
     fetch("http://localhost:3000/films")
      .then(response => response.json())
        // handle the response
@@ -16,6 +59,12 @@ function fetchData() {
             const main = document.getElementById("film-item");
             const p = document.createElement('p') 
             const bt = document.createElement('button') 
+            bt.id =char.id
+            p.addEventListener('click', ()=>{
+                main.remove()
+
+                deleteFilm(char.id)
+            })
             // we have create variable p and assigne all the names in an array
             // we have created variable button and assigned all the id in an array
             p.textContent = char.title;
@@ -46,14 +95,38 @@ function fetchData() {
      }
 function displayFilmsDetails(e) {
 
-    const id =e.target.id
+    const id = e.target.id
     fetchFilms(id)
 }
 
-function f(params) {
+function fetchFilms(id) {
+    fetch(`http://localhost:3000/films/${id}`)
+    .then(response => response.json())
+    .then(json=> createFilm(json))
     
 }
 
+function createFilm(films) {   
+    
+        const l2 =document.getElementById("film-details")
+        const p1 = document.createElement('p')
+        l2.innerHTML=films.title
+        l2.append(p1)
+        const l3 = document.getElementById("run");
+        const p2 = document.createElement('p') 
+        l2.innerHTML=films.title
+        l3.append(p2)
+        const l4 = document.getElementById("show");
+        const p3 = document.createElement('p') 
+        l4.innerHTML=films.showtime
+        l4.append(p3)
+        const l5 = document.getElementById("showtime");
+        const image = document.createElement('img') 
+        image.src = films.poster
+        l5.append(image)
+
+   
+}
 
 
 }
